ee74a5533079bb61aae46920e08efae5dc6800d8
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2022-04-13 15:11:33 -0300

    Algorithm class project

---
+++ Delivery/on_demand.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct dados{
+    int distancias[10][100];//distancia do local x para o y (grafo)
+    int entregas[10][100];//entrega do local x com peso y
+    int dist_sup[10];//distancia do supermercado do entregador x
+    int dist_local[10][10];//distancia do entregador x ao local y
+};
+
+int main(){
+    //declaracao de variaveis
+    dados info;//struct para informacoes
+    int n_locais;//numero de locais
+    int n_entregadores;//numero de entregadores
+    
+
+    //input dos locais e suas distancias(grafo)
+    //input do numero de entregadores e suas distancias do supermercado
+    //input do local e peso de cada entrega
+
+    //calculo de distancia e tempo de cada entregador ate o supermercado e ate cada casa
+    
+    //calculo de quais pacotes irao com cada entregador
+
+    //output de quais compras ficaram com cada entregadro
+    //output do tempo percorrido por cada entregador
+    //output da recuperacao de caminho de cada entregador
+
+    return 0;
+}
