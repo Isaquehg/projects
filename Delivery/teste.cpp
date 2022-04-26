676d09dc0e3360fa23bd66f95a474758742e2cb1
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2022-04-26 17:21:56 -0300

    update

--- Delivery/teste.cpp
+++ Delivery/teste.cpp
@@ -1,151 +1,178 @@
 #include <iostream>
+#include <list>
+#include <climits>
 
 using namespace std;
 
-struct dados1{
-    int distancia[100]; //distancia ate o local x
-    int menor_distancia[100]; //menor distancia ate o local x
-    int peso; //peso da entrega do local
-    bool entregue;//verificar se a compra desse local ja foi entregue
+struct no{
+    int v;//vertice
+    int distancia;//distancia aresta
+};
+struct info{
+    int peso;
+    int dist[100];
+    int entregue;
+};
+struct entrega{
+    int mochila;
+    int tempo_total;
+    int local_atual;
+    list<int> entregou;
+    list<int> caminho;
 };
 
-struct dados2{
-    int dist_sup;//distancia do entregador ao supermercado em MINUTOS
-    int dist_local[100];//distancia do entregador ao local y
-    int tempo;//tempo total gasto pelo entregador
-    int custo;//custo p comparar com outro entregadores
-    int localidade;//local atual do entregador
-    int mochila;//peso das entregas
-    int caminho[100];//caminhos que o entregador passou
-    int n_entregas;//numero de entregas que o entregador fez
-};
-
-void menor_caminho(dados1 local[], int n_locais){
-    int proxima_cidade;
-    bool passou[100];
-    for(int i = 0; i < n_locais; i ++){ //zerando
-        passou[i] = false;
-    }
-    //assumindo que a menor distancia do local i ao j é em linha reta
-    for(int i = 0; i < n_locais; i ++){
-        for(int j = 0; j < n_locais - 1; j ++){
-            local[i].menor_distancia[j] = local[i].distancia[j];
-        }
-    }
-    int menor_atual;
-    int menor_dist[100][100];//
-    for(int i = 0; i < (n_locais - 1); i ++){//ver o menor caminho para todos os locais
-
-        for(int j = i + 1; j < n_locais; j ++){//ate o local j
-            menor_atual = local[i].distancia[j];
-            if(j == ){
-
-            }
-            else{
-                //recursivo que vai iterando por todas as cidades
-            }
-        }
-    }
+void caminho_entregador(int local, int entregador){
+    entrega motoboy[100];
+    motoboy[entregador].caminho.push_back(local);
 }
 
-void quem_leva(dados1 local[], dados2 entregador[], int n_locais, int n_entregadores){
-    int menor_custo = 10000;
-    int entregador_escolhido;
-    int peso_atual;
-    int local_atual;
-    int tempo_atual;
-    for(int i = 1; i < n_locais; i ++){
-        for(int j = 0; j < n_entregadores; j ++){
-            entregador[j].custo += local[entregador[j].localidade].distancia[i];
-            if((entregador[j].custo < menor_custo) && (entregador[j].mochila + local[i].peso < 18)){
-                entregador_escolhido = j;
-                peso_atual = local[i].peso;
-                local_atual = i;
-                tempo_atual = local[entregador[j].localidade].distancia[i];
+void cria_aresta(list<no>adj[], int u, int v, int d){
+    no aui;//auxiliar
+    //criando arestas
+    aui.v = v;
+    aui.distancia = d;
+    adj[u].push_back(aui);
+    //rota inversa(nao orientado)
+    aui.v = u;
+    adj[v].push_back(aui);
+}
+void dijkstra(list<no>adj[], int nVertices, int start, int end, int &custo, int entregador, list<int> caminho[], int x[]){
+    int v;//vertice
+    custo = 0;
+    int destino;//vertice de destino
+    int weight;//distancia da aresta analisada
+    int dist;//distancia auiiliar
+    int u;//aui
+    int parent[100];//pai do vertice
+    int distancia[100];//vetor p distancias
+    bool intree[100];//se esta na arvore
+    list<int> stack;//pilha p resultado
+    list<no>::iterator p;
+    for(u = 0; u < nVertices; u ++){
+        intree[u] = false;
+        distancia[u] = INT_MAX;
+        parent[u] = -1;
+    }
+    distancia[start] = 0;
+    v = start;
+    while(intree[v] == false){
+        intree[v] = true;
+        for(p = adj[v].begin(); p != adj[v].end(); p ++){
+            destino = p->v;
+            weight = p->distancia;
+            if(distancia[destino] > distancia[v] + weight){//dijkstra
+                distancia[destino] = distancia[v] + weight;
+                parent[destino] = v;
             }
         }
-        entregador[entregador_escolhido].localidade = local_atual;
-        entregador[entregador_escolhido].mochila += peso_atual;
-        entregador[entregador_escolhido].tempo += tempo_atual;
-        entregador[entregador_escolhido].caminho[entregador[entregador_escolhido].n_entregas] = local_atual;
-        entregador[entregador_escolhido].n_entregas += 1;
-        for(int z = 0; z < n_locais; z ++){
-            entregador[entregador_escolhido].dist_local[z] = local[local_atual].distancia[z];
+
+        v = 0;
+        dist = INT_MAX;
+        for(u = 0; u < nVertices; u ++){
+            if((intree[u] == false) && (dist > distancia[u])){
+                dist = distancia[u];
+                v = u;
+            }
         }
-        //output de quais compras ficaram com cada entregador
-        cout << "Entregador " << entregador_escolhido << " entregou para o local " << local_atual << endl;
-        cout << "Tempo : " << entregador[entregador_escolhido].tempo << endl;
     }
-    //output do tempo percorrido por cada entregador
-    for(int i = 0; i < n_entregadores; i ++){
-        cout << "Tempo gasto pelo entregador " << i << ": " << entregador[i].tempo;
+    //output
+    cout << "Menor caminho: ";
+    stack.push_front(end);
+    u = end;
+    while(parent[u] != -1){
+        stack.push_front(parent[u]);
+        u = parent[u];
+    }
+    x = 0;
+    while(!stack.empty()){
+        x[entregador] ++;
+        caminho[entregador].push_front(*stack.begin());
+        cout << *stack.begin() << " ";
+        stack.pop_front();
+    }
+    cout << endl;
+    custo = distancia[end];
+    cout << "Custo: " << custo << endl;
+}
+
+int main(){
+    int n_locais, n_entregadores;//numero de vertices e de entregadores
+    int vo, vf;//vertices inicial e final
+    int i, j, d, p;//local inicial, final, distancia dessa aresta e peso da entrega
+    int x;//auxiliar
+    int custo_atual;//var p comparacao de custos
+    info local[100];//informacoes do local
+    entrega motoboy[100];//dados base p entregador
+    list<int> caminho[100];//caminho do entregador
+    list<no> adj[100];//lista adjacencia
+
+    //INPUT
+    //criacao do grafo
+    cout << "Entre com o numero de locais e de entregadores: ";
+    cin >> n_locais >> n_entregadores;
+    cout << endl;
+    for(int i = 0; i < (n_locais - 1); i ++){
+        for(int j = i + 1; j < n_locais; j ++){
+            cout << "Entre com a distancia do local " << i << " ao local " << j << " em minutos: ";
+            cin >> d;
+            cout << endl;
+            local[i].dist[j] = d;
+            cria_aresta(adj, i, j, d);
+        }
+    }
+    //peso das entregas
+    for(int i = 1; i < n_locais; i ++){
+        cout << "Entre com o peso da entrega do local " << i << ": ";
+        cin >> p;
         cout << endl;
-        cout << "Caminho do entregador " << i << ": 0 ";
-        //output para recuperacao de caminho de cada entregador
-        for(int j = 0; j < entregador[i].n_entregas; j ++){
-            cout << entregador[i].caminho[j] << " ";
+        local[i].peso = p;
+    }
+    //distancia ate o supermercado de cada entregador, atualizacao de local e resetar distancia da mochila
+    for(int i = 0; i < n_entregadores; i ++){
+        cout << "Entre com a distancia do entregador " << i << " ate o supermercado: ";
+        cin >> x;
+        motoboy[i].tempo_total += x;
+        motoboy[i].local_atual = 0;
+        motoboy[i].mochila = 0;
+    }
+
+    int menor = INT_MAX;
+    int motoboy_escolhido;
+    int n_locais_atual[100];
+    for(int i = 1; i < n_locais; i ++){
+        for(int j = 0; j < n_entregadores; j ++){    
+            //motoboy[j].local_atual >> i;
+            dijkstra(adj, n_locais, motoboy[j].local_atual, i, custo_atual, j, caminho, n_locais_atual);
+            if(custo_atual < menor && (motoboy[j].mochila + local[i].peso <= 18)){
+                motoboy_escolhido = j;
+            }
+            for(int z = 0; z < n_locais_atual[j]; z ++){
+                caminho[z].pop_front();//remover do caminho percorrido caso nao seja o entregador escolhido
+            }
+        }
+        motoboy[motoboy_escolhido].local_atual = i;//atualizacao do local atual do entregador
+        motoboy[motoboy_escolhido].mochila += local[i].peso;//peso gasto
+        motoboy[motoboy_escolhido].tempo_total += local[motoboy[motoboy_escolhido].local_atual].dist[i];//distancia gasta
+        motoboy[motoboy_escolhido].entregou.push_front(i);//compra entregue pelo motoboy
+        menor = INT_MAX;
+    }
+
+    //OUTPUT
+    for(int j = 0; j < n_entregadores; j ++){
+        for(int i = 0; i < n_locais_atual[j]; i ++){
+            cout << "O caminho do entregador " << i << " foi: ";
+            cout << *caminho[i].begin() << " - ";
+            caminho[i].pop_front();
+        }
+        cout << endl;
+        cout << "O tempo total percorrido pelo entregador " << j << " foi de: " << motoboy[i].tempo_total << endl;
+        cout << "As compras entregues pelo entregador " << j << " foram: ";
+        while(!motoboy[i].entregou.empty()){
+            cout << *motoboy[j].entregou.begin() << " - ";
+            motoboy[j].entregou.pop_front();
         }
         cout << endl;
     }
     
-}
-
-int main(){
-    //declaracao de variaveis
-    dados1 local[100];//informacoes base de determinado local(grafo)
-    dados2 entregador[100];//informacoes dos entregadores
-    int n_locais;//numero de locais
-    int n_entregadores;//numero de entregadores
-
-    //input numero de locais e numero de entregadores
-    cout << "Entre com o numero de locais e com o numero de entregadores: ";
-    cin >> n_locais >> n_entregadores;
-    cout << endl;
-    //input dos locais e suas distancias(grafo)
-    cout << "Entrada do Grafo(O primeiro local sera o supermercado)" << endl;
-    for(int i = 0; i < (n_locais - 1); i ++){
-        for(int j = (i + 1); j < n_locais; j ++){
-            cout << "Entre com a distancia do local " << i << " ate o local " << j << ": ";
-            cin >> local[i].distancia[j];
-            local[j].distancia[i] = local[i].distancia[j];//caminho reverso
-            cout << endl;
-        }
-    }
-
-    //distancias dos entregadores ate o supermercado
-    int m = 10000;//menor distancia
-    int motoboy;//entregador c menor distancia
-    for(int i = 0; i < n_entregadores; i ++){
-        cout << "Entre com a distancia do entregador " << i << " ate o supermercado em minutos: ";
-        cin >> entregador[i].dist_sup;
-        if(entregador[i].dist_sup < m){
-            m = entregador[i].dist_sup;
-            motoboy = i;
-        }
-    }
-
-    //input do local e peso de cada entrega
-    //obs.: Contador comeca do 1, pois o local 0 é o supermercado!
-    local[0].peso = 0;
-    for(int i = 1; i < n_locais; i ++){
-        cout << "Entre com o peso da entrega do local " << i << ": ";
-        cin >> local[i].peso;
-    }
-
-    //reset
-    int passou[100];//verificar se ja passou no local
-    for(int i = 0; i < n_locais; i ++){//resetando visitas aos locais
-        passou[i] = false;
-        local[i].entregue = false;
-    }
-    for(int i = 0; i < n_entregadores; i ++){//somando tempo ao supermercado no custo total
-        entregador[i].tempo = entregador[i].dist_sup;
-        entregador[i].n_entregas = 0;
-        entregador[i].mochila = 0;
-    }
-  
-    //calculo do tempo de cada entregador ate cada casa passando pelo supermercado
-    quem_leva(local, entregador, n_locais, n_entregadores);
-
     return 0;
 }
